0dee3a8236f463d6bbf58010b66e8c12c5e3c3e3
Alexey Ryabov <lesha_12_01@mail.ru>
2020-10-05 14:15:11 +0300

    fix: scroll retains at the bottom on page transition

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { use100vh } from 'react-div-100vh';
 
 import mainPhoto from 'static/photo.jpg';
 import Header from 'components/header';
@@ -11,42 +12,53 @@ import * as css from './hero-section.module.scss';
 export type HeroSectionProps = React.HTMLAttributes<HTMLDivElement>;
 
 function HeroSection(props: HeroSectionProps) {
-  return (
-    <div {...props} className={css.section}>
-      <Header variant="light" />
+  const height = use100vh();
 
-      <div className={css.heroBox}>
-        <Container className={css.heroBoxContainer}>
-          <div className={css.heroImgBox}>
-            <img className={css.heroImg} src={mainPhoto} />
-          </div>
-          <div>
-            <h1 className={css.heroTitle}>Индивидуальный гид по Санкт-Петербургу</h1>
-            <div className={css.heroCta}>
-              <Button
-                as="a"
-                className={css.heroBtn}
-                href={`#${EXCURSIONS_SECTION.id}`}
-                isRounded
-              >
-                Экскурсии
-              </Button>
-              <Button
-                className={css.heroBtn}
-                variant="outlined"
-                mainColor="white"
-                isRounded
-              >
-                Обо мне
-              </Button>
+  const content = useMemo(
+    () => (
+      <>
+        <Header variant="light" />
+
+        <div className={css.heroBox}>
+          <Container className={css.heroBoxContainer}>
+            <div className={css.heroImgBox}>
+              <img className={css.heroImg} src={mainPhoto} />
             </div>
-          </div>
-        </Container>
-      </div>
+            <div>
+              <h1 className={css.heroTitle}>Индивидуальный гид по Санкт-Петербургу</h1>
+              <div className={css.heroCta}>
+                <Button
+                  as="a"
+                  className={css.heroBtn}
+                  href={`#${EXCURSIONS_SECTION.id}`}
+                  isRounded
+                >
+                  Экскурсии
+                </Button>
+                <Button
+                  className={css.heroBtn}
+                  variant="outlined"
+                  mainColor="white"
+                  isRounded
+                >
+                  Обо мне
+                </Button>
+              </div>
+            </div>
+          </Container>
+        </div>
 
-      <div className={css.chevronDown}>
-        <ChevronDown />
-      </div>
+        <div className={css.chevronDown}>
+          <ChevronDown />
+        </div>
+      </>
+    ),
+    [],
+  );
+
+  return (
+    <div {...props} style={{ minHeight: height || '100vh' }} className={css.section}>
+      {content}
     </div>
   );
 }
